9e4c60117c4be38cff875255cd32b0d3975c7ae1
hong <you@example.com>
2023-03-28 12:47:02 +0900

    4주차 수정

--- week4/1.py
+++ week4/1.py
@@ -75,12 +75,38 @@ print(var[1:5],var[2:4],var[0:3],var[0:6])
 print('python'[0:len('python')])
 print(var[:3],var[:4],var[1:],var[3:])
 
-시험에 안나오는 부분
+###시험에 안나오는 부분
 print(var[-5:-1],var[-4:-1],var[-6:-3],var[-6:-1],var[-len('var'):-1])
-
-'''
 
 str = 'Monty Python'
 print(len(str))
 print(str[0:5],str[:6],str[6:12])
 print(str[-12:-7],str[-6:],str[-6:0])
+print(str[::-1],str[0:6:2])
+
+###(str[::x] x가 양수일 때는 x칸 만큼 간격을 조정 음수일때는 x칸 만큼 역순로 간격을 조정)
+
+print("abcdef" + "\b" + "c")
+print("hello\n world")
+print("aaa\vbbb\vccc")
+print("aaaa\'bbbb\'cccc")
+
+str_var="하하하하하"
+### type(str_var) =>  str
+print(str_var.replace("하","호"))
+
+str_var2 = "안녕하세요.파이썬. 파이썬 수업입니다."
+str_var3=(str_var2.replace("파이썬","자바"))
+print("str_var2",str_var2)
+print("str_var3",str_var3)
+
+'''
+
+# 실수를 입력 받음.
+# 102.345
+# 1+0+2+3+4+5
+#sum을 출력하라
+
+num_str=input("실수를 입력해주세요")
+sum=num_str.replace(".","")
+print(int(sum[0])+int(sum[1])+int(sum[2])+int(sum[3])+int(sum[4])+int(sum[5]))
