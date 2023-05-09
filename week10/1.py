6d791ace86f6ccbd8dd1ec564f2406047369f60f
byj <20220961@dongyang.com>
2023-05-09 12:36:08 +0900

    10week python

---
+++ week10/1.py
@@ -0,0 +1,23 @@
+### 모듈 (module)
+### 함수나 변수, 클래스 정의 등 파이썬 코드가 저장된 소스파일
+### 모듈은 파이썬 프로그램에서 불러와 사용 ( #import == #include )
+import sys
+import numpy as np
+import math
+
+'''
+print(sys.builtin_module_names)
+from math import gcd, fsum
+
+arrA = np.array([1,2,3,4,5]) #list=[1,2,3,4,5]
+arrB = np.array([6,7,8,9,10]) # np = numpy
+print(arrA + arrB)
+print(arrA - arrB)
+print(arrA * arrB)
+print(math.gcd(10,20))
+print(math.fsum([1,2,3]))
+print(math.ceil(5.333))
+'''
+
+import per as p
+p.hololive()
---
+++ week10/per.py
@@ -0,0 +1,9 @@
+#per.py
+
+def hololive() :
+    print("holoJP , holoEN, holoID")
+
+if __name__ == "__main__" :
+    print("per.py에서 작동했습니다.")
+else :
+    print("per.py에서 가져왔습니다.")
